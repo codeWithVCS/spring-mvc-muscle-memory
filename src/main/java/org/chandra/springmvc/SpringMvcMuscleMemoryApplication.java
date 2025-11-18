f38ba5e317855008b7c8ce133fe2666a91e5b0e1
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2025-11-18 07:07:13 +0530

    feat: initialize project structure – base setup for Spring MVC muscle memory

---
+++ src/main/java/org/chandra/springmvc/SpringMvcMuscleMemoryApplication.java
@@ -0,0 +1,13 @@
+package org.chandra.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMuscleMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcMuscleMemoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chandra/springmvc/SpringMvcMuscleMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chandra.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcMuscleMemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
