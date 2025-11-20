e3fcb584be49f820e535f110b09279d2745e3921
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2025-11-20 19:16:08 +0530

    feat: implement task-3.8 – set cookie using ResponseEntity

---
+++ src/main/java/org/chandra/springmvc/topic3_request_response/task8/CookieController.java
@@ -0,0 +1,22 @@
+package org.chandra.springmvc.topic3_request_response.task8;
+
+import jakarta.servlet.http.Cookie;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CookieController {
+
+    @GetMapping("/api/v1/set-cookie")
+    public ResponseEntity<String> setUserCookie(){
+        Cookie cookie = new Cookie("user-id", "12345");
+        cookie.setPath("/");
+       cookie.setHttpOnly(true);
+        HttpHeaders headers = new HttpHeaders();
+        String cookieValue = cookie.getName() + "=" + cookie.getValue() + "; Path= " + cookie.getPath() + "; HttpOnly=" + cookie.isHttpOnly();
+        headers.add(HttpHeaders.SET_COOKIE, cookieValue);
+       return ResponseEntity.ok().headers(headers).body("Cookie set successfully");
+    }
+}
---
+++ src/main/java/org/chandra/springmvc/topic3_request_response/task8/TaskRunner.java
@@ -0,0 +1,11 @@
+package org.chandra.springmvc.topic3_request_response.task8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskRunner.class, args);
+    }
+}
